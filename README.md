75a5cd03e6d1d512e47709e80b4247f3c0684c72
Amogh Shetkar <ashetkar@yugabyte.com>
2024-09-23 10:59:51 +0000

    Add restart feature

--- README.md
+++ README.md
@@ -12,6 +12,18 @@
     ```
 - Usage
     ```shell
-    mvn exec:java -Dexec.mainClass=com.yugabyte.app.App -Dexec.args="[--create-table] [--sessions <num>] [--threads <num>] [--queries <num>] [--datafile <path>] [--rows <num>]"
+    YBDB_PATH=<path/to/YugabyteDB/dir> mvn exec:java -Dexec.mainClass=com.yugabyte.app.App -Dexec.args="--restart-nodes [--nodes <num>] [--pause-time <seconds>]"
+    # OR
+    mvn exec:java -Dexec.mainClass=com.yugabyte.app.App -Dexec.args="--perform-reads [--create-table] [--sessions <num>] [--threads <num>] [--queries <num>] [--datafile <path>] [--rows <num>] ]
     ```
-- Currently, it only performs reads on a table.
+
+    - `--restart-nodes` - Continuously stop/start a random node.
+        - `--nodes <num>` - Total number of nodes available in cluster to choose from. Default is 3.
+        - `--pause-time` - Time to pause in between stopping/starting a node. Default is 15 seconds.
+
+    - `--perform-reads` - Run SELECT operations as per given options.
+        - `--create-table` - Create the SensorData table (currently, needs to be populated separately).
+        - `--sessions` - Total number of sessions to create.
+        - `--threads` - Total number of threads to create for each session. Default 1.
+        - `--queries` - Total number of queries each thread will perform. Default is 1.
+        - `--rows` - Total number of rows in the table available for querying so that the app can randomly do SELECTs in this range. Default is 10.
--- src/main/java/com/yugabyte/app/App.java
+++ src/main/java/com/yugabyte/app/App.java
@@ -4,7 +4,14 @@ import com.datastax.driver.core.*;
 import com.datastax.driver.core.policies.DefaultRetryPolicy;
 import com.yugabyte.driver.core.policies.PartitionAwarePolicy;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
@@ -17,14 +24,89 @@ public class App {
   private static boolean createTable = false;
   private static int queriesPerThreadPerSession = 1;
   private static int numOfRows = 10;
+  private static String path;
+  private static int nodes = 3;
+  private static int pauseSeconds = 15;
 
-  public static void main(String[] args) {
-    readWorkloadTest(args);
+  private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+  public static void main(String[] args) throws InterruptedException {
+
+    if (args == null || args.length < 1) {
+      System.out.println("Usage:\n  mvn exec:java -Dexec.mainClass=com.yugabyte.app.App -Dexec.args=\"--restart-nodes [--nodes <num>] [--pause-time <seconds>]\"\n"
+        + "  # OR\n"
+        + "  mvn exec:java -Dexec.mainClass=com.yugabyte.app.App -Dexec.args=\"--read-workload [--create-table] [--sessions <num>] [--threads <num>] [--queries <num>] [--datafile <path>] [--rows <num>]\"");
+      return;
+    }
+    if (args[0].equals("--restart-nodes")) {
+      path = System.getenv("YBDB_PATH");
+      if (path == null || path.trim().isEmpty()) {
+        throw new IllegalStateException("YBDB_PATH not defined.");
+      }
+      stopStartNodes(args);
+    } else {
+      readWorkloadTest(args);
+    }
+  }
+
+  private static void stopStartNodes(String[] args) throws InterruptedException {
+    processNodeRestartArgs(args);
+    System.out.println("Restarting nodes continuously ...");
+    Random rand = new Random();
+    while (true) {
+      int nodeIndex = rand.nextInt(nodes) + 1;
+      // stop a node
+      executeCmd(path + "/bin/yb-ctl stop_node " + nodeIndex, "Stop node " + nodeIndex, 10);
+      // pause a bit
+      Thread.sleep(pauseSeconds * 1000);
+      // start the same node
+      executeCmd(path + "/bin/yb-ctl start_node " + nodeIndex, "Start node " + nodeIndex, 10);
+      boolean isAlive = false;
+      do {
+        try {
+          executeCmd(path + "/bin/ycqlsh 127.0.0." + nodeIndex + " -e \"SELECT * FROM SYSTEM.LOCAL;\"", "Host 127.0.0." + nodeIndex + " is ready", 5);
+          isAlive = true;
+        } catch (RuntimeException e) {
+          Thread.sleep(2000);
+        }
+      } while (!isAlive);
+      Thread.sleep(pauseSeconds * 1000);
+    }
+  }
+
+  private static void processNodeRestartArgs(String[] args) {
+    for (int i = 1; i < args.length; i++) {
+      System.out.println("Arg #" + i + ": " + args[i]);
+      switch (args[i]) {
+        case "--nodes":
+          try {
+            nodes = Integer.parseInt(args[++i]);
+            if (nodes < 1 || nodes > 120) {
+              throw new IllegalArgumentException("Invalid number of nodes: " + nodes);
+            }
+            System.out.println("Nodes in the cluster: " + nodes);
+          } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid number of nodes: " + nodes);
+          }
+          break;
+        case "--pause-time":
+          try {
+            pauseSeconds = Integer.parseInt(args[++i]);
+            if (pauseSeconds < 1) {
+              throw new IllegalArgumentException("Invalid pause interval: " + pauseSeconds);
+            }
+            System.out.println("Pause interval: " + pauseSeconds);
+          } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid pause interval: " + pauseSeconds);
+          }
+          break;
+      }
+    }
   }
 
   // For circuit breaker testing
   private static void readWorkloadTest(String[] args) {
-    System.out.println("Usage:\n  mvn exec:java -Dexec.mainClass=com.yugabyte.app.App -Dexec.args=\"[--create-table] [--sessions <num>] [--threads <num>] [--queries <num>] [--datafile <path>] [--rows <num>]\"");
+    System.out.println("Usage:\n  mvn exec:java -Dexec.mainClass=com.yugabyte.app.App -Dexec.args=\"--read-workload [--create-table] [--sessions <num>] [--threads <num>] [--queries <num>] [--datafile <path>] [--rows <num>]\"");
     System.out.println("Read Workload Test");
 
     processArgs(args);
@@ -135,23 +217,36 @@ public class App {
     Thread[][] threads = new Thread[numOfSessions][numOfThreads];
     for (int i = 0; i < numOfSessions; i++) {
       final int fi = i;
-      PreparedStatement prepared = sessions[i].prepare(query);
       for (int j = 0; j < numOfThreads; j++) {
+        final int fj = j;
         threads[fi][j] = new Thread(new Runnable() {
           @Override
           public void run() {
-            for (int k = 0; k < queriesPerThreadPerSession; k++) {
-              int r = rand.nextInt(numOfRows);
-              ResultSet rs = sessions[fi].execute(prepared.bind("customer" + r, r));
-              for (Row row : rs) {
-                row.getString("customer_name");
-                row.getInt("device_id");
+            try {
+              PreparedStatement prepared = sessions[fi].prepare(query);
+              for (int k = 0; k < queriesPerThreadPerSession; k++) {
+                int r = rand.nextInt(numOfRows);
+                ResultSet rs = sessions[fi].execute(prepared.bind("customer" + r, r));
+                for (Row row : rs) {
+                  String name = row.getString("customer_name");
+                  int device = row.getInt("device_id");
+                  if (name != null && device > 0) {}
+                }
+                if (k % 50 == 0 && k > 0) {
+                  System.out.println("Thread " + fj + " ran " + k + " queries so far");
+                }
               }
+              System.out.println("Completed queries for thread " + fj);
+            } catch (Throwable e) {
+              System.out.println("Exception while selects for thread " + fj + ": " + e.getMessage());
+              e.printStackTrace();
+              System.exit(1);
             }
           }
         });
         threads[fi][j].start();
       }
+      System.out.println("Started all threads for session " + i);
     }
 
     for (int i = 0; i < numOfSessions; i++) {
@@ -176,4 +271,29 @@ public class App {
     // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     // Populate the table via ycqlsh
   }
+
+    public static void executeCmd(String cmd, String msg, int timeout) {
+      try {
+        System.out.println("Command to be executed: " + cmd);
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.command("sh", "-c", cmd);
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+        process.waitFor(timeout, TimeUnit.SECONDS);
+        int exitCode = process.exitValue();
+        if (exitCode != 0) {
+          String result = new BufferedReader(new InputStreamReader(process.getInputStream()))
+                  .lines().collect(Collectors.joining("\n"));
+          throw new RuntimeException(msg + ": FAILED\n" + result);
+        }
+        System.out.println(msg + ": SUCCEEDED!");
+      } catch (Exception e) {
+        // if (rethrow && e instanceof RuntimeException) {
+        //   throw (RuntimeException) e;
+        // } else {
+          System.out.println("Exception " + e);
+        // }
+      }
+    }
+
 }
